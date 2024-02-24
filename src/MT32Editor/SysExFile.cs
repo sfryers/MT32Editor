b9f606ba0c701bc8971f7a939a9178f90514ce35
sfryers <121967111+sfryers@users.noreply.github.com>
2024-02-24 16:15:41 +0000

    Restored redundant class SysExFile.cs due to CodeQL errors

---
+++ src/MT32Editor/SysExFile.cs
@@ -0,0 +1,10 @@
+﻿namespace MT32Edit;
+
+/// <summary>
+/// REDUNDANT CLASS - methods formerly in this class can now be found in LoadSysExFile.cs and SaveSysExFile.cs
+/// </summary>
+internal static class SysExFile
+{
+    // MT32Edit: SysExFile class (REDUNDANT)
+    // S.Fryers Feb 2024 
+}
