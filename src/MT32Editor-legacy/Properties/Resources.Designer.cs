26ae16c51a076a6a870069c4ec69ab5998586f8c
sfryers <121967111+sfryers@users.noreply.github.com>
2024-04-20 15:19:42 +0100

    Updated resources file

--- src/MT32Editor-legacy/Properties/Resources.Designer.cs
+++ src/MT32Editor-legacy/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MT32Edit.Properties {
+namespace MT32Edit_legacy.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MT32Edit.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MT32Edit.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MT32Edit_legacy.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
